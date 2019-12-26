12184fc36ae566f1b3cc168d464ed698494a0bd5
Minh <16520742@gm.uit.edu.vn>
2019-12-27 04:50:24 +0700

    Revert "Thêm đường dẫn + Load tin tức theo danh mục"
    This reverts commit 223eb909c9aac9f3d6da6fa94e52346402507936.

--- Src/OnlineShop/OnlineShop/Controllers/ContentController.cs
+++ Src/OnlineShop/OnlineShop/Controllers/ContentController.cs
@@ -30,7 +30,7 @@ namespace OnlineShop.Controllers
             ViewBag.Last = totalPage;
             ViewBag.Next = page + 1;
             ViewBag.Prev = page - 1;
-
+       
             return View(model);
         }
 
@@ -43,16 +43,16 @@ namespace OnlineShop.Controllers
             ViewBag.tag = new ContentDao().ListAllTag();
             return View(model);
         }
-        public ActionResult Category(long id)
+
+        public ActionResult Tag(string tagId, int page = 1, int pageSize = 10)
         {
-            int page = 1; int pageSize = 10;
-            var model = new ContentDao().ListAllByCategory(id);
+            var model = new ContentDao().ListAllByTag(tagId, page, pageSize);
             int totalRecord = 0;
+
             ViewBag.Total = totalRecord;
-            ViewBag.Page = 1;
-            ViewBag.category = new CategoriesDao().ListAll();
-            ViewBag.recentpost = new ContentDao().ListRecentPost();
-            ViewBag.tags = new ContentDao().ListAllTag();
+            ViewBag.Page = page;
+
+            ViewBag.Tag = new ContentDao().GetTag(tagId);
             int maxPage = 5;
             int totalPage = 0;
 
@@ -63,10 +63,7 @@ namespace OnlineShop.Controllers
             ViewBag.Last = totalPage;
             ViewBag.Next = page + 1;
             ViewBag.Prev = page - 1;
-
             return View(model);
         }
     }
 }
-
-      
